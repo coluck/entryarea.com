199641d9b4b5922f49a739e00912a1739ff1368c
goeswog <goeswog@gmail.com>
2019-07-06 00:13:43 +0300

    robots txt

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from app.core import views
+
 
 app_name = 'core'
 
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt",
+                                            content_type="text/plain")),
     path('s', views.search),
     path('contact/', views.contact, name='contact'),
     path('set_timezone/', views.set_timezone, name='timezone'),
